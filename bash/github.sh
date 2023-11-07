567fc3e30a4f41c589bb575f6f83ec308f490c6b
LuisGRR <lgerardo685@gmail.com>
2023-11-06 22:52:21 -0600

    Git gestor

--- bash/github.sh
+++ bash/github.sh
@@ -3,24 +3,35 @@
 echo "Hello"
 
 if command -v git >/dev/null 2>&1 ; then
-echo "Input accion for repository 1 = clone, 2 = update"
-read num
+    echo "Input action for repository 1 = clone, 2 = pull"
+    read num
     case $num in 
-    1) 
-        echo "Input for repository" 
-        read repo
-        git clone $repo
-        ;;
-    2)
-        pwd
-        echo "Directory entry to update repository, example: Repositori/"
-        read paht
-        cd $paht
-        git pull
-        ;;
-    *)
-        echo "Input numbre accion"
+        1) 
+            echo "Input URL for repository to clone" 
+            read repo
+            if git clone $repo; then
+                echo "Repository cloned successfully"
+            else
+                echo "Failed to clone repository"
+            fi
+            ;;
+        2)
+            echo "Directory entry to update repository, example: Repositori/"
+            read path
+            if [ -d "$path" ]; then
+                cd $path
+                if git pull; then
+                    echo "Repository updated successfully"
+                else
+                    echo "Failed to update repository"
+                fi
+            else
+                echo "Directory does not exist"
+            fi
+            ;;
+        *)
+            echo "Invalid input. Please enter 1 to clone or 2 to update a repository."
     esac
 else 
-    echo "Git not install"
+    echo "Git is not installed. Please install Git to use this script."
 fi
