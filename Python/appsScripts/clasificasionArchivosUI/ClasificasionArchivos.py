f93236182f71eb9ccd212bd43747a3c6b65bff68
LuisGRR <lgerardo685@gmail.com>
2024-05-30 00:37:58 -0600

    Optimizasion de codigo

--- Python/appsScripts/clasificasionArchivosUI/ClasificasionArchivos.py
+++ Python/appsScripts/clasificasionArchivosUI/ClasificasionArchivos.py
@@ -5,12 +5,13 @@ import logging
 
 dirExtenciones = set()
 diccionarioArchivos = []
+archivos_fallidos = []
 
-#configuracion de loggion
-logging.basicConfig(filename="ClasificasionArchivos.log",level=logging.INFO)
+# configuracion de loggion
+logging.basicConfig(filename="ClasificasionArchivos.log", level=logging.INFO)
 
-#funciones del programa
 
+# funciones del programa
 def extencionArchivo(dirCarpeta):
     for fichero in os.scandir(dirCarpeta):
         if fichero.is_file():
@@ -19,38 +20,58 @@ def extencionArchivo(dirCarpeta):
                 dirExtenciones.add(extencion)
     return dirExtenciones
 
+
 def buacarArchivos(dirCarpeta):
     for extencion in dirExtenciones:
         buscarArchivoExtencion(extencion, dirCarpeta)
     return len(diccionarioArchivos)
 
+
 def buscarArchivoExtencion(extencion, dirCarpeta):
-     for fichero in os.scandir(dirCarpeta):
+    for fichero in os.scandir(dirCarpeta):
         if fichero.is_file() and fichero.name.endswith(extencion):
             diccionarioArchivos.append((extencion, fichero.name))
+
 
 def crearCarpetasExtenciones(dirCarpeta):
     try:
         crearCarpetaRaiz(dirCarpeta)
         for ext in dirExtenciones:
-            os.mkdir(dirCarpeta+'Clasificasion-archivos/Archivos-'+ext)
+            os.makedirs(
+                f"{dirCarpeta}Clasificasion-archivos/Archivos-{ext}", exist_ok=True
+            )
         return True
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            logging.error("Error: %s", e)
-            return False
+    except FileExistsError as e:
+        logging.error(f"Error: {e}")
+        return False
+    except Exception as err:
+        logging.error(f"Error: {err}")
+
 
 def crearCarpetaRaiz(dirCarpeta):
     try:
-        os.mkdir(dirCarpeta+'Clasificasion-archivos')
-    except OSError as e:
-          if e.errno != errno.EEXIST:
-            logging.error("Error: %s",e)
+        os.makedirs(f"{dirCarpeta}Clasificasion-archivos", exist_ok=True)
+    except FileExistsError as e:
+        if e.errno != errno.EEXIST:
+            logging.error(f"Error: {e}")
             raise
+    except Exception as err:
+        logging.error(f"Error: {err}")
 
-def moverArchivosExtencion(dirCarpeta,extencion,name):
+
+def moverArchivosExtencion(dirCarpeta, extencion, name):
     try:
-        destino = os.path.join(dirCarpeta, 'Clasificasion-archivos', 'Archivos-'+extencion, name)
+        destino = os.path.join(
+            dirCarpeta, "Clasificasion-archivos", f"Archivos-{extencion}", name
+        )
         shutil.move(os.path.join(dirCarpeta, name), destino)
-    except (FileNotFoundError, OSError) as err:
-        logging.error("Error: %s",err)
+        diccionarioArchivos.remove((extencion, name))
+    except FileNotFoundError as err:
+        logging.error(f"Archivo no encontrado: {err}")
+        archivos_fallidos.append((extencion, name))
+    except PermissionError as err:
+        logging.error(f"Error de permiso: {err}")
+        archivos_fallidos.append((extencion, name))
+    except Exception as err:
+        logging.error(f"Error: {err}")
+        archivos_fallidos.append((extencion, name))
